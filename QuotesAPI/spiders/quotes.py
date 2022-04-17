2cf0ccd5e0c868aba43a3e03d4910ccc7d69eb3a
Juma Melvine <juma.study@gmail.com>
2022-04-17 12:51:28 +0300

    Handled Pagination

--- QuotesAPI/spiders/quotes.py
+++ QuotesAPI/spiders/quotes.py
@@ -4,12 +4,12 @@ import json
 
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
-    allowed_domains = ['qoutes.toscrape.com']
     start_urls = ['https://quotes.toscrape.com/api/quotes?page=1']
 
     def parse(self, response):
         resp = json.loads(response.body)
         quotes = resp.get("quotes")
+        has_next = resp.get("has_next")
         
         for quote in quotes:
             yield{
@@ -17,3 +17,12 @@ class QuotesSpider(scrapy.Spider):
                 "tags"  :   quote.get("tags"),
                 "text"  :   quote.get("text")
             }
+
+    
+        if has_next:
+            next_page_number = resp.get("page") + 1
+            
+            yield scrapy.Request(
+                url = f'https://quotes.toscrape.com/api/quotes?page={next_page_number}',
+                callback = self.parse
+            )
